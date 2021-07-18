472f2e4a2b754ffe336465d4ce8abaad400b3d1c
bana118 <bana.titech@gmail.com>
2021-07-18 17:59:11 +0900

    use hooks in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Search } from "./components/Search";
 
-function App(): JSX.Element {
+const App = (): JSX.Element => {
   return (
     <div className="container mx-auto">
       <Search />
     </div>
   );
-}
+};
 
 export default App;
