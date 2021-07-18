1827c428d8e4c8c02035eb533c8ad70273743ce6
bana118 <bana.titech@gmail.com>
2021-07-18 17:15:38 +0900

    add tailwind and stylelint

---
+++ src/components/Search.tsx
@@ -0,0 +1,11 @@
+export const Search = (): JSX.Element => {
+  return (
+    <div>
+      <input
+        className="flex align-middle"
+        type="text"
+        placeholder="Search..."
+      />
+    </div>
+  );
+};
