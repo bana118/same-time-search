e36d3238592b15bc760aae0bc4e442347de5ebd8
bana118 <bana.titech@gmail.com>
2021-07-20 18:50:37 +0900

    open options page by clicking button

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ export const App = (): JSX.Element => {
   return (
     <div className="container mx-auto">
       <Search />
+      <button
+        onClick={() => {
+          chrome.runtime.openOptionsPage();
+        }}
+      >
+        設定画面
+      </button>
     </div>
   );
 };
