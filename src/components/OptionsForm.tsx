79d71d41bcaa2c6702c1b59492b0cff1f886dbc9
bana118 <bana.titech@gmail.com>
2021-07-24 18:23:18 +0900

    remove button for options page

--- src/components/OptionsForm.tsx
+++ src/components/OptionsForm.tsx
@@ -6,29 +6,42 @@ import {
   optionsSchema,
   saveOptions,
 } from "../utils/options";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { IoAdd } from "react-icons/io5";
+import { IoAdd, IoClose } from "react-icons/io5";
 
 type OptionsFormProps = {
   className?: string;
 };
 
 export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
-  const [loadedOptions, setLoadedOptions] = useState<Options | null>(null);
   const [showTooltip, setShowTooltip] = useState(false);
 
   const {
     register,
+    control,
     handleSubmit,
+    setValue,
     formState: { errors },
   } = useForm<Options>({
     resolver: zodResolver(optionsSchema),
+    defaultValues: {
+      pages: [
+        {
+          url: "https://google.com",
+          stringInputElement: "",
+        },
+      ],
+    },
+  });
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "pages",
   });
 
   useEffect(() => {
     loadOptions((options) => {
-      setLoadedOptions(options);
+      setValue("pages", options.pages, { shouldValidate: true });
     });
   }, []);
 
@@ -39,66 +52,17 @@ export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
   };
 
   const addPage = () => {
-    if (loadedOptions == null) return;
-    const newPage: Options["pages"][0] = {
-      url: "https://google.com",
-      stringInputElement: "",
-    };
-    setLoadedOptions({ pages: [...loadedOptions.pages, newPage] });
+    append({ url: "https://google.com", stringInputElement: "" });
+  };
+
+  const removePage = (index: number) => {
+    remove(index);
   };
 
   return (
     <div className={className} onSubmit={handleSubmit(save)}>
-      <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
-        {loadedOptions != null &&
-          loadedOptions.pages.map((option, index) => {
-            return (
-              <div key={index}>
-                <div className="mb-4">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="url"
-                  >
-                    {optionsLabel.url}
-                  </label>
-                  <input
-                    className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="url"
-                    placeholder="https://google.com"
-                    defaultValue={option.url}
-                    {...register(`pages.${index}.url`)}
-                  />
-                  {errors.pages?.[index]?.url?.message && (
-                    <p className="text-xs italic text-red-500">
-                      {errors.pages[index]?.url?.message}
-                    </p>
-                  )}
-                </div>
-                <div className="mb-6">
-                  <label
-                    className="block mb-2 text-sm font-bold text-gray-700"
-                    htmlFor="input"
-                  >
-                    {optionsLabel.stringInputElement}
-                  </label>
-                  <textarea
-                    className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                    id="input"
-                    placeholder='<input type="text" class="form-control" id="search" />'
-                    rows={3}
-                    defaultValue={option.stringInputElement}
-                    {...register(`pages.${index}.stringInputElement`)}
-                  />
-                  {errors.pages?.[index]?.stringInputElement?.message && (
-                    <p className="text-xs italic text-red-500">
-                      {errors.pages[index]?.stringInputElement?.message}
-                    </p>
-                  )}
-                </div>
-              </div>
-            );
-          })}
-        <div className="flex items-center">
+      <form>
+        <div className="flex flex-col items-center mb-4">
           <button
             className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
             type="submit"
@@ -107,14 +71,74 @@ export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
             Save
           </button>
           <div className={showTooltip ? "" : "invisible"}>
-            <span className="ml-2 text-green-500 bg-gray-100 rounded shadow-lg tooltip">
+            <span className="text-sm text-green-500 rounded shadow-lg tooltip">
               Saved!
             </span>
           </div>
         </div>
+        {fields.map((option, index) => {
+          return (
+            <div
+              key={index}
+              className="px-8 py-6 mb-4 bg-white rounded shadow-md"
+            >
+              <div className="flex">
+                <p className="text-sm">{index + 1}</p>
+                <button
+                  className="ml-auto"
+                  type="button"
+                  onClick={() => removePage(index)}
+                >
+                  <IoClose size={24} />
+                </button>
+              </div>
+              <div className="mb-4">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="url"
+                >
+                  {optionsLabel.url}
+                </label>
+                <input
+                  className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="url"
+                  placeholder="https://google.com"
+                  defaultValue={option.url}
+                  {...register(`pages.${index}.url`)}
+                />
+                {errors.pages?.[index]?.url?.message && (
+                  <p className="text-xs italic text-red-500">
+                    {errors.pages[index]?.url?.message}
+                  </p>
+                )}
+              </div>
+              <div className="mb-6">
+                <label
+                  className="block mb-2 text-sm font-bold text-gray-700"
+                  htmlFor="input"
+                >
+                  {optionsLabel.stringInputElement}
+                </label>
+                <textarea
+                  className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                  id="input"
+                  placeholder='<input type="text" class="form-control" id="search" />'
+                  rows={3}
+                  defaultValue={option.stringInputElement}
+                  {...register(`pages.${index}.stringInputElement`)}
+                />
+                {errors.pages?.[index]?.stringInputElement?.message && (
+                  <p className="text-xs italic text-red-500">
+                    {errors.pages[index]?.stringInputElement?.message}
+                  </p>
+                )}
+              </div>
+            </div>
+          );
+        })}
       </form>
       <button
-        className="flex items-center ml-auto mr-auto bg-blue-500 border rounded-full w-9 h-9"
+        className="flex ml-auto mr-auto bg-blue-500 rounded-full"
         onClick={addPage}
       >
         <IoAdd size={36} color="white" />
--- src/options/OptionsPage.tsx
+++ src/options/OptionsPage.tsx
@@ -5,7 +5,9 @@ export const OptionsPage = (): JSX.Element => {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center m-3">
-        <h1 className="mb-2 text-xl text-center">Same Time Search</h1>
+        <h1 className="mb-2 text-xl font-bold text-center">
+          Same Time Search Options
+        </h1>
         <OptionsForm className="w-9/12" />
       </div>
     </div>
