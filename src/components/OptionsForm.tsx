ecb8aeac2a9f648e0ffeb54746fe6d6b39085a8d
bana118 <bana.titech@gmail.com>
2021-07-24 15:45:16 +0900

    use map in options form

--- src/components/OptionsForm.tsx
+++ src/components/OptionsForm.tsx
@@ -14,8 +14,7 @@ type OptionsFormProps = {
 };
 
 export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
-  const [url, setUrl] = useState("");
-  const [stringInputElement, setStringInputElement] = useState("");
+  const [loadedOptions, setLoadedOptions] = useState<Options | null>(null);
   const [showTooltip, setShowTooltip] = useState(false);
 
   const {
@@ -28,8 +27,7 @@ export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
 
   useEffect(() => {
     loadOptions((options) => {
-      setUrl(options.pages[0].url);
-      setStringInputElement(options.pages[0].stringInputElement);
+      setLoadedOptions(options);
     });
   }, []);
 
@@ -41,62 +39,72 @@ export const OptionsForm = ({ className }: OptionsFormProps): JSX.Element => {
 
   return (
     <div className={className} onSubmit={handleSubmit(save)}>
-      <form className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
-        <div className="mb-4">
-          <label
-            className="block mb-2 text-sm font-bold text-gray-700"
-            htmlFor="url"
-          >
-            {optionsLabel.url}
-          </label>
-          <input
-            className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            id="url"
-            placeholder="https://google.com"
-            defaultValue={url}
-            {...register("pages.0.url")}
-          />
-          {errors.pages?.[0]?.url?.message && (
-            <p className="text-xs italic text-red-500">
-              {errors.pages?.[0].url.message}
-            </p>
-          )}
-        </div>
-        <div className="mb-6">
-          <label
-            className="block mb-2 text-sm font-bold text-gray-700"
-            htmlFor="input"
-          >
-            {optionsLabel.stringInputElement}
-          </label>
-          <textarea
-            className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-            id="input"
-            placeholder='<input type="text" class="form-control" id="search" />'
-            rows={3}
-            defaultValue={stringInputElement}
-            {...register("pages.0.stringInputElement")}
-          />
-          {errors.pages?.[0]?.stringInputElement?.message && (
-            <p className="text-xs italic text-red-500">
-              {errors.pages?.[0].stringInputElement.message}
-            </p>
-          )}
-        </div>
-        <div className="flex items-center">
-          <button
-            className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
-            type="submit"
-            onBlur={() => setShowTooltip(false)}
-          >
-            Save
-          </button>
-          <div className={showTooltip ? "" : "invisible"}>
-            <span className="ml-2 text-green-500 bg-gray-100 rounded shadow-lg tooltip">
-              Saved!
-            </span>
-          </div>
-        </div>
+      <form>
+        {loadedOptions != null &&
+          loadedOptions.pages.map((option, index) => {
+            return (
+              <div
+                key={index}
+                className="px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md"
+              >
+                <div className="mb-4">
+                  <label
+                    className="block mb-2 text-sm font-bold text-gray-700"
+                    htmlFor="url"
+                  >
+                    {optionsLabel.url}
+                  </label>
+                  <input
+                    className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    id="url"
+                    placeholder="https://google.com"
+                    defaultValue={option.url}
+                    {...register(`pages.${index}.url`)}
+                  />
+                  {errors.pages?.[index]?.url?.message && (
+                    <p className="text-xs italic text-red-500">
+                      {errors.pages[index]?.url?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="mb-6">
+                  <label
+                    className="block mb-2 text-sm font-bold text-gray-700"
+                    htmlFor="input"
+                  >
+                    {optionsLabel.stringInputElement}
+                  </label>
+                  <textarea
+                    className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                    id="input"
+                    placeholder='<input type="text" class="form-control" id="search" />'
+                    rows={3}
+                    defaultValue={option.stringInputElement}
+                    {...register(`pages.${index}.stringInputElement`)}
+                  />
+                  {errors.pages?.[index]?.stringInputElement?.message && (
+                    <p className="text-xs italic text-red-500">
+                      {errors.pages[index]?.stringInputElement?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex items-center">
+                  <button
+                    className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
+                    type="submit"
+                    onBlur={() => setShowTooltip(false)}
+                  >
+                    Save
+                  </button>
+                  <div className={showTooltip ? "" : "invisible"}>
+                    <span className="ml-2 text-green-500 bg-gray-100 rounded shadow-lg tooltip">
+                      Saved!
+                    </span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
       </form>
     </div>
   );
