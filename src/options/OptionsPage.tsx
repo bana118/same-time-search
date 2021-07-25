d98dc66c5890faaf5f3a565f5d334289e53303ae
bana118 <bana.titech@gmail.com>
2021-07-25 18:38:29 +0900

    select search group by select box

--- src/options/OptionsPage.tsx
+++ src/options/OptionsPage.tsx
@@ -7,6 +7,9 @@ export const OptionsPage = (): JSX.Element => {
         <h1 className="mb-2 text-xl font-bold text-center">
           Same Time Search Options
         </h1>
+        <h2 className="mb-2 text-lg font-bold text-center">
+          Caution: The data being edited will be deleted when you switch the tab
+        </h2>
         <GroupTabs className="w-9/12" />
       </div>
     </div>
